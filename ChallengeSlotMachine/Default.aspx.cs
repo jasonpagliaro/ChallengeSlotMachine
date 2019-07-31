c35b6b8b425b75362509c91e9d612130044abc13
Jason Pagliaro <jasonpagliaro@gmail.com>
2019-07-30 20:33:34 -0600

    Start of Win Condition logic

--- ChallengeSlotMachine/Default.aspx.cs
+++ ChallengeSlotMachine/Default.aspx.cs
@@ -24,16 +24,24 @@ namespace ChallengeSlotMachine
             {
                 SpinResultLabel.Text = "Please enter a valid dollar amount!";
             }
-            
-            SpinImage1.ImageUrl = GetRandomImageURL();
-            SpinImage2.ImageUrl = GetRandomImageURL();
-            SpinImage3.ImageUrl = GetRandomImageURL();
 
+            string image1 = GetRandomImageName();
+            string image2 = GetRandomImageName();
+            string image3 = GetRandomImageName();
+
+            SpinImage1.ImageUrl = "/Images/" + image1 + ".png";
+            SpinImage2.ImageUrl = "/Images/" + image2 + ".png";
+            SpinImage3.ImageUrl = "/Images/" + image3 + ".png";
+
+            string winCondition = null;
             //TODO Evaluate images
 
             //TODO Determine win/loss
-
-            //TODO Find payout modifier
+            if (DetermineWinOrLose(image1, image2, image3, out winCondition))
+            {
+                //TODO Find payout modifier
+                DeterminePayout();
+            }
 
             //TODO Calculate winnings (if applicable)
 
@@ -49,6 +57,7 @@ namespace ChallengeSlotMachine
 
         }
 
+
         private bool GetBet(out int currentBet)
         {
             if (!int.TryParse(BetTextBox.Text, out currentBet))
@@ -63,10 +72,56 @@ namespace ChallengeSlotMachine
             currentWalletTotalLabel.Text = walletTotal.ToString();
         }
 
-        private string GetRandomImageURL()
+        private string GetRandomImageName()
         {
             string[] imageNames = new string[12] { "Bar", "Bell", "Cherry", "Clover", "Diamond", "HorseShoe", "Lemon", "Orange", "Plum", "Seven", "Strawberry", "Watermellon" };
-            return "/Images/" + imageNames[random.Next(0, imageNames.Length)] + ".png";
+            return imageNames[random.Next(0, imageNames.Length)];
+        }
+        private bool DetermineWinOrLose(string image1, string image2, string image3, out string winCondition)
+        {
+            winCondition = null;
+
+            if (image1 != "Bar" && image2 != "Bar" && image3 != "Bar")
+            {
+                if (image1 == "Cherry" || image2 == "Cherry" || image3 == "Cherry")
+                {
+                    winCondition = "Cherry";
+                    return true;
+                }
+                else if (image1 == "Seven" && image2 == "Seven" && image3 == "Seven")
+                {
+                    winCondition = "Sevens";
+                    return true;
+                }
+                else
+                    return false;
+            }
+            return false;
+        }
+
+        private int DeterminePayout()
+        {
+            if (winCondition == "Sevens")
+                return 100;
+
+            else if (winCondition == "Cherry")
+            {
+                int cherryCount = 0;
+
+                if (image1 == "Cherry")
+                    cherryCount++;
+
+                if (image2 == "Cherry")
+                    cherryCount++;
+
+                if (image2 == "Cherry")
+                    cherryCount++;
+
+                return cherryCount++;
+            }
+
+            else
+                return 0;
         }
 
     }
